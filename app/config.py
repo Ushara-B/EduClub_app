7fc6487fd5b64640f5ccd79ba0c04fec698dc6d3
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-12-27 14:01:41 +0530

    update in config

--- app/config.py
+++ app/config.py
@@ -3,4 +3,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///courseclubDB.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 's54fjrjfhto099857845g47ybfjhfhfrvfrfkfgf'
-    ALLOWED_ORIGINS = "*"
